e64ad9226009dbf056d47833c7c27fa642089be8
danyial <danny.smolinsky@googlemail.com>
2016-03-15 21:41:25 +0100

    Added files via upload

--- install.sh
+++ install.sh
@@ -1,49 +1,18 @@
 #!/bin/bash
 
 IPADDRESS=$(whiptail --title "IP-Adresse" --inputbox "Bitte geben Sie die IP-Adresse der Zentrale ein" 10 60 3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "IP-Adresse:" $IPADDRESS
-else
-    echo "Abbruch"
-fi
 
 USERNAME=$(whiptail --title "Benutzername" --inputbox "Bitte geben Sie Ihren SmartHome Benutzernamen ein" 10 60 3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Benutzername:" $USERNAME
-else
-    echo "Abbruch"
-fi
 
 PASSWORD=$(whiptail --title "Passwort" --inputbox "Bitte geben Sie Ihr SmartHome Passwort ein" 10 60 3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Passwort:" $PASSWORD
-else
-    echo "Abbruch"
-fi
 
 VERSION=$(whiptail --title "Version" --inputbox "Bitte geben Sie die Softwareversion der Zentrale ein" 10 60 1.70 3>&1 1>&2 2>&3)
- 
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    echo "Version:" $VERSION
-else
-    echo "Abbruch"
-fi
 
-sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install gcc python-dev
-sudo apt-get install python-pip
 
 cd ~/iSmartHome
 
-sed -i -e "s/\(DAEMON_OPTS=\).*/\1""-i $IPADDRESS -u $USERNAME -p $PASSWORD -v $VERSION""/" ismarthome.sh
+sed -i -e "s/\(DAEMON_OPTS=\).*/\1'-i $IPADDRESS -u $USERNAME -p $PASSWORD -v $VERSION'/" ismarthome.sh
 
 git clone https://github.com/giampaolo/psutil.git
 git clone https://github.com/dlitz/pycrypto.git
@@ -54,8 +23,6 @@ sudo python setup.py install
 cd ~/iSmartHome/pycrypto
 sudo python setup.py install
 
-sudo pip install requests
-
 cd ~/iSmartHome
 sudo cp ismarthome.sh /etc/init.d/ismarthome.sh
 sudo chmod 755 ismarthome.py
@@ -63,6 +30,12 @@ sudo chmod 755 /etc/init.d/ismarthome.sh
 cd /etc/init.d
 sudo update-rc.d ismarthome.sh defaults
 
+sudo crontab -e > ismarthomecron
+echo "@daily sudo service ismarthome restart" >> ismarthomecron
+sudo crontab ismarthomecron
+rm ismarthomecron
+
+sudo reboot
 
 
 
