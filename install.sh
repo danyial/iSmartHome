794231d67c5924c4f60112448b315c54f16e9b49
danyial <danny.smolinsky@googlemail.com>
2016-03-16 07:36:17 +0100

    Added files via upload

--- install.sh
+++ install.sh
@@ -13,7 +13,16 @@ sudo apt-get install gcc python-dev
 cd /tmp/iSmartHome
 
 sed -i -e "s/\(DAEMON_OPTS=\).*/\1'-i $IPADDRESS -u $USERNAME -p $PASSWORD -v $VERSION'/" ismarthome.sh
+sed -i -e "s/\(DAEMON_USER=\).*/\1$USER/" ismarthome.sh
 
+git clone https://github.com/giampaolo/psutil.git
+git clone https://github.com/dlitz/pycrypto.git
+
+cd psutil
+sudo python setup.py install
+
+cd /tmp/iSmartHome/pycrypto
+sudo python setup.py install
 
 cd /opt
 sudo mkdir iSmartHome
@@ -25,11 +34,15 @@ sudo cp /tmp/iSmartHome/ismarthome.py /opt/iSmartHome/ismarthome.py
 
 sudo chmod 755 /opt/iSmartHome/ismarthome.py
 sudo chmod 755 /etc/init.d/ismarthome.sh
+sudo chown -R $USER /opt/iSmartHome/
 
 cd /etc/init.d
 sudo update-rc.d ismarthome.sh defaults
 
 crontab -l | { cat; echo "@daily sudo service ismarthome restart"; } | crontab -
 
+sudo systemctl daemon-reload
+
+sudo service ismarthome start
 
 
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo rm -R /opt/iSmartHome
+sudo rm /etc/init.d/ismarthome.sh
+sudo systemctl daemon-reload
