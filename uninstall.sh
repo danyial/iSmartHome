f3e9cebf42d1aa27819bf542f12501ccd8ae05e2
danyial <danny.smolinsky@googlemail.com>
2016-03-16 08:00:53 +0100

    Added files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,4 @@ sudo service ismarthome stop
 sudo rm -R /opt/iSmartHome
 sudo rm /etc/init.d/ismarthome.sh
 sudo systemctl daemon-reload
+crontab -l | sed 's/@daily sudo service ismarthome restart//' | crontab -
