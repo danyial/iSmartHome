5cd713d77e978901d0643c945bb2b42971951d18
danyial <danny.smolinsky@googlemail.com>
2016-03-24 10:03:26 +0100

    iSmartHome

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ PASSWORD=$(whiptail --title "Passwort" --inputbox "Bitte geben Sie Ihr SmartHome
 
 VERSION=$(whiptail --title "Version" --inputbox "Bitte geben Sie die Softwareversion der Zentrale ein" 10 60 1.70 3>&1 1>&2 2>&3)
 
-sudo apt-get install gcc python-dev python-requests
+sudo apt-get --assume-yes install gcc python-dev python-requests lighttpd php5-common php5-cgi php5 php5-curl
 
 cd /tmp/iSmartHome
 
@@ -28,10 +28,13 @@ cd /opt
 sudo mkdir iSmartHome
 cd iSmartHome
 sudo mkdir Logs
+cd /var/www/html
+sudo mkdir iSmartHome
 
 sudo cp /tmp/iSmartHome/ismarthome.sh /etc/init.d/ismarthome.sh
 sudo cp /tmp/iSmartHome/ismarthome.py /opt/iSmartHome/ismarthome.py
 sudo cp /tmp/iSmartHome/Actions.json /opt/iSmartHome/Actions.json
+sudo cp /tmp/iSmartHome/index.php /var/www/html/iSmartHome/index.php
 
 sudo chmod 755 /opt/iSmartHome/ismarthome.py
 sudo chmod 755 /etc/init.d/ismarthome.sh
@@ -43,7 +46,10 @@ sudo update-rc.d ismarthome.sh defaults
 crontab -l | { cat; echo "@daily sudo service ismarthome restart"; } | crontab -
 
 sudo systemctl daemon-reload
-
 sudo service ismarthome start
+
+sudo lighttpd-enable-mod fastcgi fastcgi-php
+sudo service lighttpd restart
+
 
 
